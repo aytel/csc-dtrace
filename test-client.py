a6d389b5c7576882a2779c38e5a635a8b08f6ed6
hetdalet <hetdalet@example.com>
2020-05-14 19:03:03 +0500

    Added basic client for testing

---
+++ test-client.py
@@ -0,0 +1,43 @@
+import argparse
+import random
+import requests
+
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 5000
+DEFAULT_QUERIES = 100
+HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
+OP_LIST = ['+', '-', '/', '*']
+
+
+def post(endpoint, params):
+    return requests.post(endpoint, headers=HEADERS, data=params)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s', '--host',
+        type=str, default=DEFAULT_HOST,
+        help='The host to send requests to'
+             'Default is {}'.format(DEFAULT_PORT)
+    )
+    parser.add_argument(
+        '-p', '--port',
+        type=int, default=DEFAULT_PORT,
+        help='The destination port to send requests to. '
+             'Default is {}'.format(DEFAULT_PORT)
+    )
+    parser.add_argument(
+        '-q', '--queries',
+        metavar='NQUERIES', type=int, default=DEFAULT_QUERIES,
+        help='Set the number of queries to send. '
+             'Default is {}'.format(DEFAULT_QUERIES)
+    )
+    args = parser.parse_args()
+    for i in range(args.queries):
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        op = random.choice(OP_LIST)
+        params = {'statement': f'{a}{op}{b}'}
+        response = post('http://127.0.0.1:80/api/v1.0/calculator', params)
+        print(f'{a} {op} {b} = {response.text}')
