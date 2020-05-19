22bd39577b0e7fb616bd590f77cfc74df41741ba
hetdalet <hetdalet@example.com>
2020-05-19 20:16:49 +0500

    X-Request-Id adds to logs across all services

--- test-service/service/calculator/main.py
+++ test-service/service/calculator/main.py
@@ -5,7 +5,7 @@ import flask
 import requests
 
 
-HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
+HEADERS = (('Content-Type', 'application/x-www-form-urlencoded'),)
 OP_MAP = {
     '+': operator.add,
     '-': operator.sub,
@@ -17,11 +17,41 @@ SERVICES = {
     'presenter': os.getenv('SRV_PRESENTER')
 }
 
+
+class ContextFilter(logging.Filter):
+
+    def filter(self, record):
+        record.x_request_id = flask.request.headers.get('x-request-id')
+        return True
+
+
+# extra = {'app_name':'Super App'}
+
+# logger = logging.getLogger(__name__)
+# syslog = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s %(app_name)s : %(message)s')
+# syslog.setFormatter(formatter)
+# logger.setLevel(logging.INFO)
+# logger.addHandler(syslog)
+
+
 app = flask.Flask(__name__)
 if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    # logging.basicConfig(format='%(x_request_id)s %(asctime)-15s %(name)-5s %(levelname)-8s %(message)s')
+    # gunicorn_logger = logging.getLogger('gunicorn.error')
+    # app.logger.handlers = gunicorn_logger.handlers
+    # app.logger.setLevel(gunicorn_logger.level)
+    # app.logger.addFilter(ContextFilter())
+
+
+    logger = logging.getLogger(__name__)
+    syslog = logging.StreamHandler()
+    formatter = logging.Formatter('%(x_request_id)s %(asctime)-15s %(name)-5s %(levelname)-8s %(message)s')
+    syslog.setFormatter(formatter)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(syslog)
+    logger.addFilter(ContextFilter())
+    app.logger = logger
 
 
 @app.route("/api/v1.0/calculator", methods=['POST'])
@@ -37,7 +67,9 @@ def calc():
 
 
 def post(endpoint, params):
-    return requests.post(endpoint, headers=HEADERS, data=params)
+    headers = dict(HEADERS)
+    headers['x-request-id'] = flask.request.headers.get('x-request-id')
+    return requests.post(endpoint, headers=headers, data=params)
 
 
 def parse(statement):
