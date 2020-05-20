a6ef6ab25b199e84f14989083790ef0b82662058
hetdalet <hetdalet@example.com>
2020-05-20 19:00:42 +0500

    Configure Logstash for access logs parsing

--- test-app/gunicorn/config.py
+++ test-app/gunicorn/config.py
@@ -2,4 +2,4 @@ workers = 2
 loglevel = 'info'
 errorlog = '-'
 accesslog = '-'
-access_log_format = '%({x-request-id}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '%({x-request-id}i)s %(h)s %(t)s "%(r)s" %(s)s'
