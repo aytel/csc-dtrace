b402652a7f4f50205930583e20d44d8f98efe198
hetdalet <hetdalet@example.com>
2020-05-25 20:17:17 +0500

    Add x-request-id to response headers

--- test-app/service/presenter/main.py
+++ test-app/service/presenter/main.py
@@ -1,3 +1,4 @@
+import functools
 import os
 import flask
 
@@ -5,7 +6,17 @@ import flask
 app = flask.Flask(__name__)
 
 
+def response(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        resp = flask.make_response(func(*args, **kwargs))
+        resp.headers['x-request-id'] = flask.request.headers.get('x-request-id')
+        return resp
+    return wrapper
+
+
 @app.route("/api/v1.0/presenter", methods=['POST'])
+@response
 def dispath():
     val = flask.request.form.get('value')
     frmt = flask.request.form.get('format')
