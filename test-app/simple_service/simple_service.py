5fd18fc5d40785dc733c1f6d8a983daf574b5813
hetdalet <hetdalet@example.com>
2020-05-26 03:44:22 +0500

    Add SERVICE_ID to logline

--- test-app/simple_service/simple_service.py
+++ test-app/simple_service/simple_service.py
@@ -1,15 +1,33 @@
 import functools
 import logging
 import os
+import socket
 import time
 
+import docker
 import flask
 import gunicorn
 import gunicorn.glogging
 import requests
 
 
-APP_NAME = os.getenv('APP_NAME')
+def get_service_id():
+    client = docker.from_env()
+    container_list = client.containers.list(
+        filters={'status': 'running','id': socket.gethostname()},
+        ignore_removed=True
+    )
+    if container_list:
+        this = container_list[0]
+    else:
+        raise Exception("Can't get service_id")
+    project = this.labels['com.docker.compose.project']
+    service = this.labels['com.docker.compose.service']
+    number = this.labels['com.docker.compose.container-number']
+    return f'{project}_{service}_{number}'
+
+
+SERVECE_ID = get_service_id()
 HEADERS = (('Content-Type', 'application/x-www-form-urlencoded'),)
 
 
@@ -34,9 +52,9 @@ class GunicornLogger(gunicorn.glogging.Logger):
         return t_str.format(msec)
 
 
-app = flask.Flask(APP_NAME)
+app = flask.Flask(SERVECE_ID)
 
-logger = logging.getLogger(APP_NAME)
+logger = logging.getLogger(SERVECE_ID)
 app_log = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(name)s %(x_request_id)s [%(levelname)s] %(message)s')
 app_log.setFormatter(formatter)
