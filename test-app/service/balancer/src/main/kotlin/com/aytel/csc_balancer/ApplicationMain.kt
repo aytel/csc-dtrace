5f55a4d69f35aaebaaa79a103047c58b0c5347b2
Arseny Babushkin <aytelquant@yandex-team.ru>
2020-05-30 16:10:17 +0500

    impl logs format

--- test-app/service/balancer/src/main/kotlin/com/aytel/csc_balancer/ApplicationMain.kt
+++ test-app/service/balancer/src/main/kotlin/com/aytel/csc_balancer/ApplicationMain.kt
@@ -4,13 +4,26 @@ import java.nio.file.Paths
 import java.util.*
 
 import com.aytel.csc_balancer.config.Properties
+import com.aytel.csc_balancer.network.ListenThreadHandler
 import com.aytel.csc_balancer.network.ListenThreadRunnable
+import java.text.SimpleDateFormat
+import java.util.logging.ConsoleHandler
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
 
 object ApplicationMain {
     private val PROPERTY_PATH: String = System.getProperty(
         "properties",
         Paths.get(Paths.get("").toAbsolutePath().toString(), "balancer.properties").toString()
     )
+
+    const val logFormat = "%s, %s, %s, %s, %s, %s\n"
+    val timestampFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+
+    init {
+
+    }
 
     @Throws(Exception::class)
     @JvmStatic
